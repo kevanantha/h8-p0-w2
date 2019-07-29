89863f2a5158e81e02d3800ab5129536a0209de3
Kevin Anantha <kevin.anantha@gmail.com>
2019-07-29 20:08:43 +0700

    :fire:

--- proxytia.js
+++ proxytia.js
@@ -1,22 +1,22 @@
-var nama = '';
-var peran = '';
+var nama = 'Kevin';
+var peran = 'Penyihir';
 
-// Output untuk Input nama = '' dan peran = ''
-if (nama === 'Mikael' && !peran) {
+if (!nama && !peran) {
+  // Output untuk Input nama = '' dan peran = ''
+  console.log('Nama harus diisi!');
+} else if (nama && !peran) {
   // Output untuk Input nama = 'Mikael' dan peran = ''
   console.log(`Halo ${nama}, Pilih peranmu untuk memulai game!`);
-} else if (nama === 'Nina' && peran === 'Ksatria') {
+} else if (nama && peran === 'Ksatria') {
   // Output untuk Input nama = 'Nina' dan peran 'Ksatria'
   console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
   console.log(`Halo ${peran} ${nama}, kamu dapat menyerang dengan senjatamu!`);
-} else if (nama === 'Danu' && peran === 'Tabib') {
+} else if (nama && peran === 'Tabib') {
   // Output untuk Input nama = 'Danu' dan peran 'Tabib'
   console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
   console.log(`Halo ${peran} ${nama}, kamu akan membantu temanmu yang terluka.`);
-} else if (nama === 'Zero' && peran === 'Penyihir') {
+} else if (nama && peran === 'Penyihir') {
   // Output untuk Input nama = 'Zero' dan peran 'Penyihir'
   console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
   console.log(`Halo ${peran} ${nama}, ciptakan keajaiban untuk membantu kemenanganmu.`);
-} else {
-  console.log('Name harus diisi!');
 }
