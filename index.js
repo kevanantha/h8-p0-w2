cb114f098daa3218c4ba5fd8b5196d967336c8c0
Kevin Anantha <kevin.anantha@gmail.com>
2019-07-29 13:39:19 +0700

    :fire:

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const kabisat = tahun => {
-  if (tahun % 4 === 0 && tahun % 100 !== 100) {
+  if (tahun % 4 === 0 && tahun % 100 !== 0) {
     return true;
   } else {
     return false;
   }
 };
 
-console.log(kabisat(2020));
+// var baju = 0;
+// for (var baju = 0; baju < 20; baju++) {
+//   console.log(baju);
+//   // console.log((baju += i));
+// }
+
+var baju = 0;
+while (baju < 20) {
+  baju += 1;
+}
+
+console.log(baju);
