df6b5f2404600becc3ea7016c065fd8830d5ddb2
Kevin Anantha <kevin.anantha@gmail.com>
2019-07-29 14:50:20 +0700

    :fire:

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const kabisat = tahun => {
-  if (tahun % 4 === 0 && tahun % 100 !== 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
+// const kabisat = tahun => {
+//   if (tahun % 4 === 0 && tahun % 100 !== 100) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// console.log(kabisat(2400));
+// console.log(2000 % 100);
 
 // var baju = 0;
 // for (var baju = 0; baju < 20; baju++) {
@@ -12,9 +15,21 @@ const kabisat = tahun => {
 //   // console.log((baju += i));
 // }
 
-var baju = 0;
-while (baju < 20) {
-  baju += 1;
-}
+// var baju = 0;
+// while (baju < 20) {
+//   baju++;
+//   console.log(baju);
+// }
 
-console.log(baju);
+// let siswa = 0;
+// const panjang = false;
+// const kukuSiswaYangDiPeriksa = true;
+
+// while (siswa < 40) {
+//   if (kukuSiswaYangDiPeriksa == panjang) {
+//     console.log('hukum');
+//   } else {
+//     console.log('puji');
+//   }
+//   siswa++;
+// }
