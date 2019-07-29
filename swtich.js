c6da3fa7ca65e44a418a3c9a175bd8f423edd663
Kevin Anantha <kevin.anantha@gmail.com>
2019-07-29 21:46:39 +0700

    :fire:

---
+++ swtich.js
@@ -0,0 +1,62 @@
+var tanggal = 23;
+var bulan = 5;
+var tahun = 1997;
+
+switch (true) {
+  case tanggal > 0 && tanggal < 32:
+    tanggal = tanggal;
+    break;
+  default:
+    tanggal = 'Invalid Tanggal';
+}
+
+switch (bulan) {
+  case 1:
+    bulan = 'Januari';
+    break;
+  case 2:
+    bulan = 'Februari';
+    break;
+  case 3:
+    bulan = 'Maret';
+    break;
+  case 4:
+    bulan = 'April';
+    break;
+  case 5:
+    bulan = 'Mei';
+    break;
+  case 6:
+    bulan = 'Juni';
+    break;
+  case 7:
+    bulan = 'Juli';
+    break;
+  case 8:
+    bulan = 'Agustus';
+    break;
+  case 9:
+    bulan = 'September';
+    break;
+  case 10:
+    bulan = 'Oktober';
+    break;
+  case 11:
+    bulan = 'November';
+    break;
+  case 12:
+    bulan = 'Desember';
+    break;
+  default:
+    bulan = 'Invalid Bulan';
+}
+
+switch (true) {
+  case tahun >= 1900 && tahun <= 2200:
+    tahun = tahun;
+    break;
+  default:
+    tahun = 'Invalid Tahun';
+}
+
+console.log(`${tanggal} ${bulan} ${tahun}`);
