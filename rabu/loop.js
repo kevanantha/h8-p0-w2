147d870db5ad07a952f022d1270af395a488d48c
Kevin Anantha <kevin.anantha@gmail.com>
2019-07-30 17:25:19 +0700

    :fire: loop while

---
+++ rabu/loop.js
@@ -0,0 +1,14 @@
+// 1. Melakukan Looping Menggunakan While
+console.log('LOOPING PERTAMA');
+var a = 2;
+while (a <= 20) {
+  console.log(a + ' - I love coding');
+  a += 2;
+}
+
+console.log('LOOPING KEDUA');
+var b = 20;
+while (b >= 2) {
+  console.log(b + ' - I will become fullstack developer');
+  b -= 2;
+}
