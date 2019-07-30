c6188b707d6f28e15d26400560bb3616787d8fa4
Kevin Anantha <kevin.anantha@gmail.com>
2019-07-30 17:29:57 +0700

    :fire: loop for

--- rabu/loop.js
+++ rabu/loop.js
@@ -12,3 +12,14 @@ while (b >= 2) {
   console.log(b + ' - I will become fullstack developer');
   b -= 2;
 }
+
+// 2. Melakukan Looping Menggunakan For
+console.log('LOOPING PERTAMA');
+for (var i = 1; i <= 20; i++) {
+  console.log(i + ' - I love coding');
+}
+
+console.log('LOOPING KEDUA');
+for (var j = 20; j > 0; j--) {
+  console.log(j + ' - I will become fullstack developer');
+}
