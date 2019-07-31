1b2f2049cf3b58cd92e7128f6c7dc5093fea63ff
Kevin Anantha <kevin.anantha@gmail.com>
2019-07-31 19:16:25 +0700

    :fire:

---
+++ jumat/balikKata.js
@@ -0,0 +1,11 @@
+function balikKata(kata) {
+  var balik = kata.split('').reverse();
+  return balik.join();
+}
+
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
---
+++ jumat/xo.js
@@ -0,0 +1,25 @@
+function xo(str) {
+  var o = 0;
+  var x = 0;
+
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] == 'o') {
+      o++;
+    } else {
+      x++;
+    }
+  }
+
+  if (x === o) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
